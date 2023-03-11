b19f1647d96bade81f4ebd85364bb48e36b9404c
HaochenQ <haochenqi1997@gmail.com>
2023-03-12 00:35:36 +1100

    purge sensitive data

--- providers.tf
+++ providers.tf
@@ -11,8 +11,8 @@ terraform {
 
 provider "azurerm" {
   features {}
-  client_id       = "2c76a25d-e465-4fb3-9a98-3ca5ebaaf781"     // ARM_CLIENT_ID
-  client_secret   = "KcD8Q~W2BONBNdXRrPhGpJB73xMoSsDFpWLpaaQ4" //ARM_CLIENT_SECRET
-  tenant_id       = "659a4789-0ce8-48e9-a7be-3e87fab48984"     // ARM_TENANT_ID
-  subscription_id = "06200282-a505-4fd3-a173-0fb4528c56dc"     // ARM_SUBSCRIPTION_ID
+  # client_id       =    // ARM_CLIENT_ID
+  # client_secret   =  //ARM_CLIENT_SECRET
+  # tenant_id       =      // ARM_TENANT_ID
+  # subscription_id =      // ARM_SUBSCRIPTION_ID
 }
